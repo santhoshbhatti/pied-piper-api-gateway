5e49c6e26ba7c90d3e9fb6a052fae595610a2a71
Santhosh-Bhatti <santhosh.bhatti@ibm.com>
2021-12-08 15:38:47 +0530

    first commit

---
+++ src/main/java/com/pp/apigateway/PiedPiperApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.pp.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiedPiperApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiedPiperApiGatewayApplication.class, args);
+	}
+
+}
